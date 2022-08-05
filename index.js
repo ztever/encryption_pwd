de81097557b0429d49924816b3f02289485a5511
ztever <478081538@qq.com>
2022-08-05 17:50:42 +0800

    modfiy encrypt

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const crypto = require("crypto");
+
 const encryptPassword = (password, PubKey, keyId) => {
   const randKey = crypto.randomBytes(32);
   const iv = crypto.randomBytes(12);
   const rsaEncrypted = crypto.publicEncrypt(
     {
       key: Buffer.from(PubKey, "base64").toString(),
-      // @ts-ignore
       padding: crypto.constants.RSA_PKCS1_PADDING
     },
     randKey
@@ -19,16 +20,14 @@ const encryptPassword = (password, PubKey, keyId) => {
   const sizeBuffer = Buffer.alloc(2, 0);
   sizeBuffer.writeInt16LE(rsaEncrypted.byteLength, 0);
   const authTag = cipher.getAuthTag();
-  return {
-    time,
-    encrypted: Buffer.concat([
-      Buffer.from([1, keyId]),
-      iv,
-      sizeBuffer,
-      rsaEncrypted,
-      authTag,
-      aesEncrypted
-    ]).toString("base64")
-  };
+  const encrypted = Buffer.concat([
+    Buffer.from([1, keyId]),
+    iv,
+    sizeBuffer,
+    rsaEncrypted,
+    authTag,
+    aesEncrypted
+  ]).toString("base64");
+  return ["#PWD_INSTAGRAM", "4", time, encrypted].join(":");
 };
 module.exports = encryptPassword;
